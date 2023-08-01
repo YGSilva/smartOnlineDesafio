630561d0d342217715bc89b90ec6d65308159af5
Yago Silva <yago.mdk284@gmail.com>
2023-08-01 10:21:50 -0300

    Added API documentation

--- desafioDotNet/Controllers/FileController.cs
+++ desafioDotNet/Controllers/FileController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using desafioDotNet.Context;
 using System.Globalization;
 using desafioDotNet.Repository.Interfaces;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace desafio.Controllers {
     [ApiController]
@@ -21,16 +22,23 @@ namespace desafio.Controllers {
             _repository = repository;
         }
 
+        /// <summary>
+        /// Upload do arquivo CNAB.txt
+        /// </summary>
+        /// <param name="file"> Arquivo com os dados </param>
+        /// <returns> Dados criados na Base </returns>
+        /// <response code="201"> Sucesso </response>
         [HttpPost("ProcessFile")]
+        [SwaggerOperation(Summary = "Processa o arquivo de registro.", Description = "Este endpoint recebe um arquivo de texto (.txt) contendo registros, processa e insere os registros na base de dados.")]
+        [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(List<RegisterModel>))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(string))]
         public IActionResult ProcessFile(IFormFile file) {
-            if (file == null || file.Length == 0) {
+            if (file == null || file.Length == 0) 
                 return BadRequest("Nenhum arquivo enviado.");
-            }
 
-            if (!Path.GetExtension(file.FileName).Equals(".txt", System.StringComparison.OrdinalIgnoreCase)) {
+            if (!Path.GetExtension(file.FileName).Equals(".txt", System.StringComparison.OrdinalIgnoreCase)) 
                 return BadRequest("O arquivo deve ter a extensão .txt.");
-            }
-
+            
             using (var reader = new StreamReader(file.OpenReadStream())) {
 
                 var content = reader.ReadToEnd();
@@ -45,20 +53,38 @@ namespace desafio.Controllers {
             }
         }
 
+        /// <summary>
+        /// Obtém uma lista com o total de saldo em conta agrupado por tipo de operação e loja com excessão das operações que falharam.
+        /// </summary>
+        /// <returns> Lista agrupada e com saldo total </returns>
+        /// <response code="200"> Sucesso </response>
+        /// <response code="404"> Dados não encontrado na base </response>
         [HttpGet("ListWithTotalBalance")]
+        [SwaggerOperation(Summary = "Obtém uma lista com o total de saldo em conta agrupado por tipo de operação e loja com excessão das operações que falharam.", Description = "Este endpoint retorna uma lista com o total de saldo em conta agrupado por tipo de operação e loja com excessão das operações que falharam..")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(List<RegisterModel>))]
+        [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(string))]
         public async Task<ActionResult> ListWithTotalBalance() {
             var file = _repository.GetListWithTotalBalance();
             return file.Any()
                 ? Ok(file)
-                : BadRequest("NÃO EXISTE NADA NA BASE DE DADOS");
+                : NotFound("NÃO EXISTE NADA NA BASE DE DADOS");
         }
 
+        /// <summary>
+        /// Obtém uma lista com as operações que falharam.
+        /// </summary>
+        /// <returns> Lista com os dados das operações falhas </returns>
+        /// <response code="200"> Sucesso </response>
+        /// <response code="404"> Dados não encontrado na base </response>
         [HttpGet("OperationsWrong")]
+        [SwaggerOperation(Summary = "Obtém uma lista com as operações que falharam.", Description = "Este endpoint retorna uma lista com as operações que falharam, agrupadas por tipo de operação e loja.")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(List<RegisterModel>))]
+        [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(string))]
         public async Task<ActionResult> OperationsWrong() {
             var file = _repository.GetOperationsWrong();
             return file.Any()
                 ? Ok(file)
-                : BadRequest("NÃO EXISTEM OPERAÇÕES QUE FALHARAM");
+                : NotFound("NÃO EXISTEM OPERAÇÕES QUE FALHARAM");
         }
     }
 }
--- desafioDotNet/Program.cs
+++ desafioDotNet/Program.cs
@@ -4,6 +4,7 @@ using desafioDotNet.Models;
 using desafioDotNet.Utils;
 using desafioDotNet.Repository.Interfaces;
 using desafioDotNet.Repository;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,7 +21,17 @@ builder.Services.AddControllers();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c => {
+    c.SwaggerDoc("v1", new OpenApiInfo {
+        Title = "SmartOnlineDesafio",
+        Version = "v1",
+        Contact = new OpenApiContact {
+            Name = "Yago Goncalves",
+            Email = "yago.mdk284@gmail.com",
+            Url = new Uri("https://www.linkedin.com/in/yago-goncalves-da-silva/")
+        }
+    });
+});
 
 
 var app = builder.Build();
